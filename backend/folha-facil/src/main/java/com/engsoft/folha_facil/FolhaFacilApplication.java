3f0a55a12145998376063ec8e6cd6f42c9f0233d
Matheus-Spirit <matheusdiasmendespop@gmail.com>
2025-09-09 10:46:14 -0300

    initial commit

---
+++ backend/folha-facil/src/main/java/com/engsoft/folha_facil/FolhaFacilApplication.java
@@ -0,0 +1,13 @@
+package com.engsoft.folha_facil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolhaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolhaFacilApplication.class, args);
+	}
+
+}
---
+++ backend/folha-facil/src/test/java/com/engsoft/folha_facil/FolhaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engsoft.folha_facil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
